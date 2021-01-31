d8222d20ee94b8441f6593e57a145985c27ca33c
Jonathan Meriño <jmerino1204@gmail.com>
2021-01-31 11:56:45 -0500

    activity_rest_detail.xml

---
+++ app/src/main/java/com/jmb/sizzlingbites/RestDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.jmb.sizzlingbites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RestDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rest_detail)
+    }
+}
